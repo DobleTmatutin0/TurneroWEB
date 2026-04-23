8311e241cfb80bc00cf1c1b6b82b2ebdec6af96c
DobleTmatutin0 <zz_00024@hotmail.com>
2026-04-22 22:06:34 -0300

    modifies the steps to comunicate with backend

--- tw/testing/features/step_definitions/01-crear-centro-de-atencion-stepdefs.js
+++ tw/testing/features/step_definitions/01-crear-centro-de-atencion-stepdefs.js
@@ -1,6 +1,5 @@
-const assert = require('assert');
 const {Given, When, Then } = require('@cucumber/cucumber');
-
+const assert = require('assert');
 const request = require('sync-request');
 
 
@@ -10,30 +9,49 @@ Given(
         this.sistema = {
             centros: []
         }
-        return 'pending';
+        // return 'pending';
     }
 );
 
 When(
-    'Cuando el administrador ingresa los datos del centro de atención: {string}, {string}, {string}, {string} y {string}',
-    function(nombre, dirección, localidad, provincia, coordenadasStr) {
-        const [lat, lon] = coordenadasStr.split(',').map(c => {
-            parseFloat(c.trim());
-        });
-
-        const coordenadas = {
-            latitud = lat,
-            longitud = lon
-        };
-        this.coordenadas = coordenadas;
+    'el administrador ingresa los datos del centro de atención: {string}, {string}, {string}, {string} y {string}',
+    function(nombre, direccion, localidad, provincia, coordenadas) {
         
-        return 'pending';
+        this.aNewCentroDeAtencion = {
+            "nombre": nombre,
+            "direccion": direccion,
+            "localidad": localidad,
+            "provincia": provincia,
+            "coordenadas": {
+                "latitud": parseFloat(coordenadas.split(',')[0].trim()),
+                "longitud": parseFloat(coordenadas.split(',')[1].trim())
+            }
+        };
+
+        this.backendResponse = request(
+            'POST',
+            'http://tw-backend:8080/centros-de-atencion',
+            {
+                json: this.aNewCentroDeAtencion
+            }
+        );
+        
+        this.response = JSON.parse(this.backendResponse.getBody('utf8'));
+        console.log(this.response);
+        // return 'pending';
     }
 );
 
 Then(
     'el sistema responde con {int} y {string}',
-    function(status_code, status_text) {
-        return 'pending';
+    function(expectedStatus, expectedMessage) {
+        
+        
+        
+
+        assert.strictEqual(this.response.status, expectedStatus);
+        assert.strictEqual(this.response.message, expectedMessage)
+        
+        // return 'pending';
     }
 );
