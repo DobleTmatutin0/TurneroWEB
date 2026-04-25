38414fc1cd94ce54e33a6ac3efeca7db1df83208
DobleTmatutin0 <zz_00024@hotmail.com>
2026-04-25 15:36:11 -0300

    implements method 'exists by name and address' method

--- tw/backend/src/main/java/unpsjb/labprog/backend/business/service/CentroDeAtencion_Service.java
+++ tw/backend/src/main/java/unpsjb/labprog/backend/business/service/CentroDeAtencion_Service.java
@@ -1,10 +1,12 @@
 package unpsjb.labprog.backend.business.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.geo.Point;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import unpsjb.labprog.backend.business.repository.CentroDeAtencion_Repository;
+import unpsjb.labprog.backend.dto.CentroDeAtencion_DTO;
 import unpsjb.labprog.backend.model.CentroDeAtencion;
 
 @Service
@@ -15,12 +17,25 @@ public class CentroDeAtencion_Service {
     private CentroDeAtencion_Repository centroDeAtencion_Repo;
 
     @Transactional
-    public CentroDeAtencion save(CentroDeAtencion aCentroDeAtencion) {
-        return centroDeAtencion_Repo.save(aCentroDeAtencion);
+    public CentroDeAtencion save(CentroDeAtencion_DTO aCentroDeAtencion) {
+
+        Point point = new Point(
+            aCentroDeAtencion.getCoordenadas().getLatitud(),
+            aCentroDeAtencion.getCoordenadas().getLongitud()
+        );
+
+        CentroDeAtencion CentroDeAtencionToSave = new CentroDeAtencion();
+        CentroDeAtencionToSave.setNombre(aCentroDeAtencion.getNombre());
+        CentroDeAtencionToSave.setLocalidad(aCentroDeAtencion.getLocalidad());
+        CentroDeAtencionToSave.setDireccion(aCentroDeAtencion.getDireccion());
+        CentroDeAtencionToSave.setCoordenadas(point);
+        CentroDeAtencionToSave.setTelefono(aCentroDeAtencion.getTelefono());
+
+        return centroDeAtencion_Repo.save(CentroDeAtencionToSave);
     }
 
-    public boolean findByName(String aName) {
-        return centroDeAtencion_Repo.existsByName(aName);
+    public boolean findByNameAndAddress(String aName, String anAddress) {
+        return centroDeAtencion_Repo.existsByNameAndAddress(aName, anAddress);
     }
 
     
