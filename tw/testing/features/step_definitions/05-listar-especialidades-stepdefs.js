9b0837df4213d3b693bb809345d399df9a559fbf
DobleTmatutin0 <zz_00024@hotmail.com>
2026-05-12 17:51:27 -0300

    adds functionality

---
+++ tw/testing/features/step_definitions/05-listar-especialidades-stepdefs.js
@@ -0,0 +1,47 @@
+const { Given, When, Then } = require('@cucumber/cucumber');
+const assert = require('assert');
+const request = require('sync-request');
+
+Given(
+    'que existen 19 especialidades registradas en el sistema',
+    function() {
+
+    }
+);
+
+When(
+    'un usuario del sistema solicita la lista de especialidades',
+    function() {
+        this.getAllResponse = request(
+            'GET',
+            'http://backend:8080/especialidad'
+        );
+
+        this.response = JSON.parse(this.getAllResponse.getBody('utf8'));
+    }
+);
+
+Then(
+    'el sistema responde con codigo: {int} y mensaje: {string}',
+    function(expectedStatus, expectedMessage) {
+        assert.strictEqual(this.response.status, expectedStatus);
+        assert.strictEqual(this.response.message, expectedMessage);
+
+    }
+);
+
+Then(
+    'la lista contiene las siguientes especialidades:',
+    function(dataTable) {
+        const expected = dataTable.hashes();
+
+        expected.forEach(exp => {
+            const encontrado = this.response.data.find(e =>
+                e.nombre === exp.nombre &&
+                e.descripcion === exp.descripcion
+            );
+            
+            assert.ok(encontrado, `No se encontro la especialidad ${exp.nombre}`);
+        });
+    }
+);
