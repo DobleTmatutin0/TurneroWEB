be1f5e4954d422a285fb668827d6691ffa543a43
DobleTmatutin0 <zz_00024@hotmail.com>
2026-05-15 22:22:51 -0300

    adds functionality

---
+++ tw/testing/features/step_definitions/07-eliminar-especialidad-stepdefs.js
@@ -0,0 +1,35 @@
+const { Given, When, Then } = require('@cucumber/cucumber');
+const assert = require('assert');
+const request = require('sync-request');
+
+Given(
+    'que la especialidad {string} existe en el sistema',
+    function(nombre) {
+        this.getResponse = request(
+            'GET',
+            `http://backend:8080/especialidad/${nombre}`
+        );
+
+        this.especialidadToDelete = JSON.parse(this.getResponse.getBody('utf8')).data;
+    
+        assert.ok(this.especialidadToDelete, `no se encontro ${nombre}`)
+    }
+);
+
+When(
+    'el administrador elimina la especialidad {string}',
+    function() {
+        this.deleteResponse = request(
+            'DELETE',
+            `http://backend:8080/especialidad/${this.especialidadToDelete.nombre}`
+        );
+    }
+);
+
+Then(
+    'el sistema responde con el codigo: {int} y mensaje: {string}',
+    function(expectedStatus, expectedMessage) {
+        assert.strictEqual(this.response.status, expectedStatus);
+        assert.strictEqual(this.response.message, expectedMessage);
+    }
+);
