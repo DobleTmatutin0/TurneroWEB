af325f90a58976edec715ed381b001a807a80d47
DobleTmatutin0 <zz_00024@hotmail.com>
2026-05-25 16:56:34 -0300

    add endpoint calls

--- tw/testing/features/step_definitions/08-consultorio-stepdefs.js
+++ tw/testing/features/step_definitions/08-consultorio-stepdefs.js
@@ -7,15 +7,37 @@ const request = require('sync-request');
 // ===================================================
 Given(
     'que existe un centro de atención llamado {string}',
-    function() {
+    function(centro_atencion) {
+        const getResponse = request(
+            'GET',
+            `http://backend:8080/centros-de-atencion/${centro_atencion}`
+        );
 
+        this.centroToModify = JSON.parse(getResponse.getBody('utf8')).data;
+
+        assert.ok(this.centroToModify, `No se encontró ${centro_atencion}`);
     }
 );
 
 When(
     'se registra un consultorio con el número {int} y el nombre {string}',
-    function() {
-        
+    function(numero, nombre_consultorio) {
+        const newConsultorio = {
+            numero: numero,
+            nombre: nombre_consultorio
+        }
+
+        this.centroToModify.consultorios.push(newConsultorio);
+
+        const putResponse = request(
+            'PUT',
+            `http://backend:8080/centros-de-atencion/${this.centroToModify.id}`,
+            {
+                json: this.centroToModify
+            }
+        );
+
+        this.response = JSON.parse(putResponse.getBody('utf8'));
     }
 );
 
@@ -31,16 +53,21 @@ Then(
 // Listar consultorios de un centro de atención
 // ================================================
 Given(
-    'que existe un centro de atención llamado "Centro Médico Esperanza"',
-    function() {
-
+    'que existe un centro de atención llamado {string}',
+    function(centro_atencion) {
+        this.nombreCentro = centro_atencion;
     }
 );
 
 When(
     'se solicita la lista de consultorios del centro',
     function() {
-        
+        const getResponse = request(
+            'GET',
+            `http://backend:8080/centros-de-atencion/${this.nombreCentro}/consultorios`
+        );
+
+        this.response = JSON.parse(getResponse.getBody('utf8'));
     }
 );
 
@@ -49,9 +76,9 @@ Then(
     function(dataTable) {
         const expected = dataTable.hashes();
 
-        expected.foreach(exp => {
+        expected.forEach(exp => {
             const consultorio = this.response.data.find(c =>
-                c.numero === exp.numero &&
+                Number(c.numero) === Number(exp.numero) &&
                 c.nombre === exp.nombre_consultorio
             );
 
@@ -66,14 +93,19 @@ Then(
 Given(
     'que el centro de atención llamado {string} no está registrado',
     function(nombreCentroInexistente) {
-        
+        this.nombreCentro = nombreCentroInexistente;
     }
 );
 
 When(
     'se solicita la lista de consultorios del centro {string}',
-    function(nombreCentroInexistente) {
-        
+    function(_nombreCentroInexistente) {
+        const getResponse = request(
+            'GET',
+            `http://backend:8080/centros-de-atencion/${this.nombreCentro}/consultorios`
+        );
+
+        this.response = JSON.parse(getResponse.getBody('utf8'));
     }
 );
 
@@ -98,25 +130,32 @@ Given(
 When(
     'se solicita la lista completa de centros con sus consultorios',
     function() {
-        
+        const getResponse = request(
+            'GET',
+            'http://backend:8080/centros-de-atencion',
+        );
+
+        this.response = JSON.parse(getResponse.getBody('utf8'));
     }
 );
 
 Then(
+    'la lista contiene los siguientes centros y consultorios:',
     function(dataTable) {
         const expected = dataTable.hashes();
 
-        expected.foreach(exp => {
+        expected.forEach(exp => {
             const centro = this.response.data.find( c =>
-                c.centro_atencion === exp.nombre
+                c.nombre === exp.centro_atencion
             );
             assert.ok(centro, `No se encontró el centro ${exp.centro_atencion}`);
 
+            assert.ok(Array.isArray(centro.consultorios));
             const consultorio = centro.consultorios.find(c =>
-                c.numero === exp.numero &&
+                Number(c.numero) === Number(exp.numero) &&
                 c.nombre === exp.nombre_consultorio
             );
-            assert.ok(consultorio, `No se encontro el consultorio ${exp.nombre_consultorio}`);
+            assert.ok(consultorio, `No se encontró el consultorio ${exp.nombre_consultorio} del centro ${exp.centro_atencion}`);
         });
     }
 );
