d3ce2a0bbf3ad79dc86aed6d3156ab81dcd07d83
DobleTmatutin0 <zz_00024@hotmail.com>
2026-05-27 21:35:40 -0300

    add some exceptions

---
+++ tw/backend/src/main/java/unpsjb/labprog/backend/exception/InvalidDataException.java
@@ -0,0 +1,7 @@
+package unpsjb.labprog.backend.exception;
+
+public class InvalidDataException extends RuntimeException {
+    public InvalidDataException(String msg) {
+        super(msg);
+    }
+}
---
+++ tw/backend/src/main/java/unpsjb/labprog/backend/exception/NoOperationException.java
@@ -0,0 +1,7 @@
+package unpsjb.labprog.backend.exception;
+
+public class NoOperationException extends RuntimeException {
+    public NoOperationException(String msg) {
+        super(msg);
+    }
+}
